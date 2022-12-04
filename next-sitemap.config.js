87913138f77d4fc111602d29e524744585f048b1
AJ <ia.jordaan1003@gmail.com>
2022-12-04 19:27:35 +0200

    sitemap config update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -6,7 +6,6 @@ module.exports = {
   priority: 0.7,
   sitemapSize: 5000,
   generateRobotsTxt: true,
-  // Default transformation function
   transform: async (config, path) => {
     return {
       loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
