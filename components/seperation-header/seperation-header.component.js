ec0cbcb4e2ef92d55ba0de45963bee88fb37656a
AJ <ia.jordaan1003@gmail.com>
2023-12-05 07:15:32 +0200

    update christmas lights

--- components/seperation-header/seperation-header.component.js
+++ components/seperation-header/seperation-header.component.js
@@ -30,7 +30,7 @@ const SeperationHeader = ({ id, childrenLvl1, childrenLvl2 }) => {
   }
   return (
     <>
-      {/* {route != 'bicycles/[slug]'
+      {route != 'bicycles/[slug]'
         ?
         <ul className='lightrope'>
           <li />
@@ -91,7 +91,7 @@ const SeperationHeader = ({ id, childrenLvl1, childrenLvl2 }) => {
           <li />
         </ul>
         :
-        null} */}
+        null}
       <Header id={id}>
         <h2 className={`header-text-h2 
       ${route == 'home' ? homeStyles.textColour :
@@ -124,10 +124,10 @@ const Header = styled.section`
   color: rgba(255,255,255,0);
   border-color: rgb(255,255,255);
   // Comment for Christmas
-  background: var(--background-grey);
+  /* background: var(--background-grey); */
   /* background: var(--light-grey); */
   // Uncomment for Christmas
-  /* background: var(--grey); */
+  background: var(--grey);
   font-family: 'Racing Sans One', serif;
   display: flex;
   flex-direction: column;
@@ -151,6 +151,9 @@ const Header = styled.section`
     margin: 0;
     color: #ed1d22;
     font-size: 1.5rem;
-    color: #000;
+    // Comment for Christmas
+    /* color: #000; */
+    // Uncomment for Christmas
+    color: #fff;
   }
 `;
